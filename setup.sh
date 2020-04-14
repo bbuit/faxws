804bb5dce8d9fe18d1743d76d033bf0b1be6ecb3
Colcamex Resources Inc <dwarren@colcamex.com>
2020-04-13 21:25:01 -0700

    more adjustments to the setup script

--- setup.sh
+++ setup.sh
@@ -2,33 +2,31 @@
 
 #configure fax service
 
-echo "CAUTION! This script will restart Tomcat after execution."
-
 # 0. check for privilege
 if [[ $EUID -ne 0 ]]; then
      	echo "This script must be run as root" 1>&2
 	exit 1
 fi
 
-# if not already downloaded.
-
 ## 1. git
-#sudo apt install git
-#
+sudo apt install git
+
 ## 2. maven
-#sudo apt install maven
-#
+sudo apt install maven
+
 ## 3. Clone
-#git clone https://dennis_warren@bitbucket.org/openoscar/faxws.git
-#
+mkdir -p ./git
+cd ./git
+git clone https://dennis_warren@bitbucket.org/openoscar/faxws.git
+
 ## 4. Checkout
-## When release is avaialable
+## git checkout origin/release -b
 
 # 5. current MySQL pass and username
+echo "Setting passwords... "
+
 read -p "MySQL database username? " username
 read -p "MySQL database password? " passwd
-
-echo "Configuring Tomcat"
 
 perl -pi -e "s/{user}/$username/"  src/main/webapp/META-INF/context.xml
 perl -pi -e "s/{passwd}/$passwd/"  src/main/webapp/META-INF/context.xml
@@ -38,20 +36,23 @@ read -p "What username do you want to log in as in tomcat? " tomcatUser
 read -p "What password do want for $tomcatUser? " tomcatPassword
 
 # 7. build FaxWs mvn clean package
-echo "Compiling web service with maven"
+echo "Build FaxWs with Maven..."
+
 mvn clean
 mvn package
 
 # 8. Migrate mvn flyway:migrate
-echo "Migrating database"
+## cannot use on older MySQL databases. Must be 5.7 and up or this command will fail.
 mvn flyway:migrate -Dflyway.user=$username -Dflyway.password=$passwd
 
 # 9. Add pass and username to db
-echo "Updating authentication tables"
+echo "Setting authentication database..."
+
 mysql -u$username -p$passwd oscarFax -e "insert into users Values('$tomcatUser','$tomcatPassword')"
 mysql -u$username -p$passwd oscarFax -e "insert into user_roles Values('$tomcatUser','user')"
 
 # 10. Move WAR and JDBC Driver to Tomcat
+echo "Installing FaxWs..."
 
 # a. find the highest version Tomcat running on this server
 TOMCAT=$(ps aux | grep org.apache.catalina.startup.Bootstrap | grep -v grep | awk '{ print $1 }')
@@ -92,13 +93,12 @@ fi
 
 # c. move the WAR file. 
 if [ -d "${TOMCAT_PATH}" ];
-	sudo mv -f ./target/FaxWs*SNAPSHOT ${TOMCAT_PATH}/faxWs.war
+	then
+		mv -f ./target/FaxWs*SNAPSHOT.war ${TOMCAT_PATH}/faxWs.war
 fi
 
 # d. move the JDBC Jar
 if [ -d "/usr/share/${TOMCAT}/lib" ];
-	cp -f ./target/FaxWs-1.0.0-SNAPSHOT/WEB-INF/lib/mysql-connector-java*.jar /usr/share/${TOMCAT}/lib/
+	then
+		cp ./target/FaxWs-1.0.0-SNAPSHOT/WEB-INF/lib/mysql-connector-java*.jar /usr/share/${TOMCAT}/lib/
 fi
-
-# 11. Restart Tomcat
-# sudo service ${TOMCAT} restart
